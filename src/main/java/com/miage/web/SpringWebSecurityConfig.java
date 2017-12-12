883008a868fcd4fc8eb2d6ec52b8eb7c4699b225
sment <sment@hotmail.fr>
2017-12-12 07:56:08 +0100

    prepare security

--- src/main/java/com/miage/web/SpringWebSecurityConfig.java
+++ src/main/java/com/miage/web/SpringWebSecurityConfig.java
@@ -4,14 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
 @EnableWebSecurity
 public class SpringWebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//    	web.ignoring().antMatchers("/static/**");
+//    }
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
     	http.authorizeRequests().antMatchers("/**").permitAll();
     	
 //        http
@@ -24,8 +32,9 @@ public class SpringWebSecurityConfig extends WebSecurityConfigurerAdapter {
 //                .permitAll()
 //                .and()
 //            .logout()
+//            	.logoutSuccessUrl("/")
 //                .permitAll();
-    	
+//    	
     }
 
     @Autowired
