8bdfe1704562fbcc1dfaa4b243a0a8dadb069803
sment <sment@hotmail.fr>
2018-01-11 21:13:25 +0100

    rest services security

--- src/main/java/com/miage/config/SpringSecurityConfig.java
+++ src/main/java/com/miage/config/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package com.miage.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -17,90 +18,125 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 import com.miage.security.SecurityUserDetailsService;
 import com.miage.web.exception.CustomAccessDeniedHandler;
 
-@Configuration
 // TODO enable SpringSecurity
 @EnableWebSecurity
 // TODO Enable method security
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
-public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+public class SpringSecurityConfig {
 
-	// TODO enable Users in DB
-	@Autowired
-	private SecurityUserDetailsService userDetailsService;
-
-	// TODO Enable SpringSecurity
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/static/**");
-	}
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-
-		// TODO enable SpringSecurity
-//		http.authorizeRequests().antMatchers("/**").permitAll();
-
-		// TODO enable SpringSecurity
-		http.authorizeRequests().antMatchers("/").permitAll();
-		http.authorizeRequests().antMatchers("/home*").permitAll();
-		http.authorizeRequests().antMatchers("/person*").access("hasAuthority('ADMIN')");
-		http.authorizeRequests().antMatchers("/product*").access("hasAuthority('USER')");
-		http.authorizeRequests().antMatchers("/category*").access("hasAuthority('USER')");
-
-//		http.authorizeRequests().antMatchers("/rest/restcategory*").access("hasAuthority('USER')");
+    @Configuration
+    @Order(1)
+    public static class RestSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+		// TODO enable Users in DB
+		@Autowired
+		private SecurityUserDetailsService userDetailsService;
 		
-		http.authorizeRequests().antMatchers("/login/**").permitAll();
-		http.formLogin().loginPage("/login").permitAll();
-		http.logout().logoutSuccessUrl("/");
-
-		http.authorizeRequests().anyRequest().authenticated();
+        protected void configure(HttpSecurity http) throws Exception {
+        	http.authorizeRequests().antMatchers("/rest/restcategory*").access("hasAuthority('USER')").and().httpBasic();
+            http.authorizeRequests().antMatchers("/rest/**").authenticated().and().httpBasic();
+        }
+        
+		// TODO enable more things : CSRF ?
+		@Bean
+		@Override
+		public AuthenticationManager authenticationManagerBean() throws Exception {
+			return super.authenticationManagerBean();
+		}
 		
-		// TODO enable SSL
-		http.requiresChannel().antMatchers("/").requiresInsecure();
-		http.requiresChannel().antMatchers("/home").requiresInsecure();
-		http.requiresChannel().anyRequest().requiresSecure();
-
-		// TODO enable Exception handling
-		http.exceptionHandling().accessDeniedHandler(accessDeniedHandler());
-		
-		// TODO Enable session management
-		http.sessionManagement()
-        	.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
-        	.sessionFixation().migrateSession();
-		
-		// TODO manage headers
-		http.headers()
-			.frameOptions().sameOrigin()
-			.httpStrictTransportSecurity().disable();
-		
-	}
-
-	// TODO enable more things : CSRF ?
-	@Bean
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
-	}
+    	// TODO enable Users in DB
+//    	@Autowired
+//    	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//    		auth
+//    			.inMemoryAuthentication()
+//    		 		.withUser("user").password("user").roles("USER")
+//    		 		.and().withUser("admin").password("admin").roles("ADMIN");
+//    	}
+    	
+    	// TODO enable Users in DB
+    	@Override
+    	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    	}
+    }
+    
+	@Configuration
+	public static class FormLoginWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+		// TODO enable Users in DB
+		@Autowired
+		private SecurityUserDetailsService userDetailsService;
 	
-	// TODO enable Users in DB
-//	@Autowired
-//	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//		auth
-//			.inMemoryAuthentication()
-//		 		.withUser("user").password("user").roles("USER")
-//		 		.and().withUser("admin").password("admin").roles("ADMIN");
-//	}
+		// TODO Enable SpringSecurity
+		@Override
+		public void configure(WebSecurity web) throws Exception {
+			web.ignoring().antMatchers("/static/**");
+		}
 	
-	// TODO enable Users in DB
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
-	}
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+	
+			// TODO enable SpringSecurity
+//			http.authorizeRequests().antMatchers("/**").permitAll();
+	
+			// TODO enable SpringSecurity
+			http.authorizeRequests().antMatchers("/").permitAll();
+			http.authorizeRequests().antMatchers("/home*").permitAll();
+			http.authorizeRequests().antMatchers("/person*").access("hasAuthority('ADMIN')");
+			http.authorizeRequests().antMatchers("/product*").access("hasAuthority('USER')");
+			http.authorizeRequests().antMatchers("/category*").access("hasAuthority('USER')");
+	
+			http.authorizeRequests().antMatchers("/login/**").permitAll();
+			http.formLogin().loginPage("/login").permitAll();
+			http.logout().logoutSuccessUrl("/");
+	
+			http.authorizeRequests().anyRequest().authenticated();
+			
+			// TODO enable SSL
+			http.requiresChannel().antMatchers("/").requiresInsecure();
+			http.requiresChannel().antMatchers("/home").requiresInsecure();
+			http.requiresChannel().anyRequest().requiresSecure();
+	
+			// TODO enable Exception handling
+			http.exceptionHandling().accessDeniedHandler(accessDeniedHandler());
+			
+			// TODO Enable session management
+			http.sessionManagement()
+	        	.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+	        	.sessionFixation().migrateSession();
+			
+			// TODO manage headers
+			http.headers()
+				.frameOptions().sameOrigin()
+				.httpStrictTransportSecurity().disable();
+			
+		}
+		
+		// TODO enable more things : CSRF ?
+		@Bean
+		@Override
+		public AuthenticationManager authenticationManagerBean() throws Exception {
+			return super.authenticationManagerBean();
+		}
+		
+		// TODO enable Users in DB
+//		@Autowired
+//		public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//			auth
+//				.inMemoryAuthentication()
+//			 		.withUser("user").password("user").roles("USER")
+//			 		.and().withUser("admin").password("admin").roles("ADMIN");
+//		}
+		
+		// TODO enable Users in DB
+		@Override
+		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+			auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+		}
 
-//	 TODO enable Exception handling
-	@Bean
-	public AccessDeniedHandler accessDeniedHandler() {
-		return new CustomAccessDeniedHandler();
+//		 TODO enable Exception handling
+		@Bean
+		public AccessDeniedHandler accessDeniedHandler() {
+			return new CustomAccessDeniedHandler();
+		}
 	}
 
 }
